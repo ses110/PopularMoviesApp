f4a58c367c2e1f20febc40081cb2848030ee3c7f
Sergio Escoto <ses110@gmail.com>
2016-09-06 16:49:00 -0600

    Added README and fixed unit-test dependencies

---
+++ README.md
@@ -0,0 +1,8 @@
+PopularMovies App
+===========
+### Displays Movies in Grid
+Fetches the most popular or the top-rated movies from the TMDB database.
+![](PopularMovies1.png)
+  
+### Click on a Movie Poster to view the Movie's Detailed information
+![](PopularMovies2.png)
--- app/src/test/java/com/example/sergioescoto/popularmovies/MovieUtilsTest.java
+++ app/src/test/java/com/example/sergioescoto/popularmovies/MovieUtilsTest.java
@@ -1,20 +1,36 @@
 package com.example.sergioescoto.popularmovies;
 
+import android.content.Context;
+import android.net.Uri;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Created by sergioescoto on 9/3/16.
  */
-public class MovieUtilsTest {
 
+@PrepareForTest({Uri.class})
+@RunWith(PowerMockRunner.class)
+public class MovieUtilsTest {
+    @Mock Context mMockContext;
 
     @Before
     public void setUp() throws Exception {
+        PowerMockito.mockStatic(Uri.class);
+        //Uri uri = PowerMockito.mock(Uri.class);
+        Uri uri = Uri.parse("https://api.themoviedb.org/3/discover/movie?api_key=fcae63a0084cbaed783adf8d8a4770f7&sort_by=vote_average.desc");
 
+        PowerMockito.when(Uri.class, "parse", Mockito.anyString()).thenReturn(uri);
     }
 
     @After
